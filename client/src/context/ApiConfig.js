b48fa2e7db81b88ed9e8d6dfb2994c762f748f80
Sujal Polawala <sujalpolawala@gmail.com>
2025-05-19 20:14:50 +0530

    Delete client directory

--- client/src/context/ApiConfig.js
+++
@@ -1,3 +0,0 @@
-export const stripeKey = process.env.REACT_APP_STRIPE_PUBLIC_KEY;
-export const apibasekey = process.env.REACT_APP_API_BASE_URL;
-export const API_BASE_URL = `${apibasekey}/api` || 'http://localhost:5000/api';
