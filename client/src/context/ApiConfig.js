708c8c7044ec2b05a7ed03a5db0d36573f62e51d
Sujal Polawala <sujalpolawala@gmail.com>
2025-05-19 19:36:03 +0530

    Update ApiConfig.js

--- client/src/context/ApiConfig.js
+++ client/src/context/ApiConfig.js
@@ -1,4 +1,3 @@
-import { apibasekey } from "../index";
-const API_BASE_URL = `${apibasekey}/api` || 'http://localhost:5000/api';
-
-export default API_BASE_URL;
+export const stripeKey = process.env.REACT_APP_STRIPE_PUBLIC_KEY;
+export const apibasekey = process.env.REACT_APP_API_BASE_URL;
+export const API_BASE_URL = `${apibasekey}/api` || 'http://localhost:5000/api';
