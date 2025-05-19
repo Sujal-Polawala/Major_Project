52415f8f2c5282f0ab792b68883984c563d51f8e
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-19 20:40:49 +0530

    Update These File

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 // /config/db.js
 
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`mongodb+srv://sujalpolawala:24nbYHl0uvOQGeRY@e-commerces.ozhgm.mongodb.net/e-commerces
-`, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
