f5849b4dc71e63e1383fceb5a08df5a8acae51f8
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-22 17:52:07 +0530

    Update API

--- client/src/config/ApiConfig.js
+++ client/src/config/ApiConfig.js
@@ -1,3 +1,3 @@
-export const stripeKey = process.env.REACT_APP_STRIPE_PUBLIC_KEY;
+export const stripeKey = process.env.REACT_APP_STRIPE_PRUBLIC_KEY;
 export const apibasekey = process.env.REACT_APP_API_BASE_URL;
 export const API_BASE_URL = `${apibasekey}` || 'http://localhost:5000';
