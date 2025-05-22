7e22c31a059e1b9c5ca389b9e1f5578350c376ee
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-22 18:56:34 +0530

    Update Skeleton

--- client/src/skeletons/productInfoSkeletonCard.js
+++ client/src/skeletons/productInfoSkeletonCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SkeletonProductInfoCard = () => {
   return (
-    <div className="max-w-2xl mx-auto p-6 bg-white border rounded-xl shadow-lg animate-pulse space-y-4">
+    <div className="w-full mx-auto p-6 bg-white border rounded-xl shadow-lg animate-pulse space-y-4">
       <div className="h-8 bg-gray-300 rounded w-3/4"></div> {/* Title */}
       <div className="h-6 bg-gray-300 rounded w-1/2"></div> {/* Price */}
       <div className="h-6 bg-gray-300 rounded w-1/3"></div> {/* Quantity */}
