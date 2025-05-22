bf9b320279458d2b4ea4a1615392e18fe570225d
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-22 18:48:52 +0530

    Add Skeleton Card

---
+++ client/src/skeletons/productInfoSkeletonCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SkeletonProductInfoCard = () => {
+  return (
+    <div className="max-w-2xl mx-auto p-6 bg-white border rounded-xl shadow-lg animate-pulse space-y-4">
+      <div className="h-8 bg-gray-300 rounded w-3/4"></div> {/* Title */}
+      <div className="h-6 bg-gray-300 rounded w-1/2"></div> {/* Price */}
+      <div className="h-6 bg-gray-300 rounded w-1/3"></div> {/* Quantity */}
+      <div className="h-20 bg-gray-300 rounded w-full"></div> {/* Description */}
+      <div className="h-6 bg-gray-300 rounded w-1/4"></div> {/* Category */}
+      <div className="h-12 bg-gray-300 rounded w-full mt-4"></div> {/* Button */}
+    </div>
+  );
+};
+
+export default SkeletonProductInfoCard;
