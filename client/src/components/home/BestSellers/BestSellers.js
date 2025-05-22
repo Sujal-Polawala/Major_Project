a96a111a8f192dfb3e0663d3511d17597b5a78c8
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-22 18:32:59 +0530

    Add Skeleton Card Loader

--- client/src/components/home/BestSellers/BestSellers.js
+++ client/src/components/home/BestSellers/BestSellers.js
@@ -1,53 +1,92 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import Slider from "react-slick";
 import Heading from "../Products/Heading";
 import Product from "../Products/Product";
-import axios from "axios"; // Import axios for API calls
+import axios from "axios";
+import SampleNextArrow from "./SampleNextArrow";
+import SamplePrevArrow from "./SamplePrevArrow";
 import { API_BASE_URL } from "../../../config/ApiConfig";
+import SkeletonCard from "../../../skeletons/productSkeletonCard";
 
 const BestSellers = () => {
-  const [bestSellers, setBestSellers] = useState([]); // State to store bestseller products
-  const [loading, setLoading] = useState(true); // Loading state to show a loader while fetching data
+  const [bestSellers, setbestSellers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  // Fetch the bestseller data when the component mounts
   useEffect(() => {
     const fetchBestSellers = async () => {
       try {
         const response = await axios.get(`${API_BASE_URL}/api/products/men`); // Replace with your backend API URL
-        setBestSellers(response.data); // Set the fetched data to state
+        setbestSellers(response.data); // Set the fetched data to state
+        setLoading(false);
       } catch (error) {
-        console.error("Error fetching bestseller products", error);
-      } finally {
-        setLoading(false); // Stop loading once data is fetched
+        console.error("Error fetching new arrival products", error);
+        setLoading(false);
       }
     };
-
     fetchBestSellers();
-  }, []); // Empty dependency array to run the effect only once when the component mounts
+  }, []);
+
+  const settings = {
+    infinite: false, // Change to false to test
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+    ],
+  };
 
   return (
-    <div className="w-full pb-20">
-      <Heading heading="Our Bestsellers" />
-      
-      {loading ? (
-        <div>Loading...</div> // Show loading text while fetching data
-      ) : (
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-3 xl:grid-cols-4">
-          {bestSellers.slice(0,4).map((product) => (
-            <Product
-              key={product.id} // Use product ID as key
-              _id={product._id}
-              image={product.image} // Replace with the actual image path from the API response
-              title={product.title}
-              price={product.price}
-              badge={product.badge}
-              des={product.description}
-              category={product.category}
-            />
-          ))}
-        </div>
-      )}
+    <div className="w-full pb-16">
+      <Heading heading="New Arrivals" />
+      <Slider {...settings}>
+        {loading
+          ? [...Array(4)].map((_, index) => (
+              <div key={index} className="px-2">
+                <SkeletonCard />
+              </div>
+            ))
+          : bestSellers.map((product) => (
+              <div className="px-2" key={product.id}>
+                <Product
+                  _id={product._id}
+                  image={product.image}
+                  title={product.title}
+                  price={product.price}
+                  badge={product.badge}
+                  des={product.description}
+                  category={product.category}
+                />
+              </div>
+            ))}
+      </Slider>
     </div>
   );
 };
 
-export default BestSellers; 
+export default BestSellers;
