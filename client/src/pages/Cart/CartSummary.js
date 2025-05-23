94623566b40e4dedb20ba498a57e3c7debf28b39
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-23 17:55:08 +0530

    Update Cart Summary and add Shipping Charge

--- client/src/pages/Cart/CartSummary.js
+++ client/src/pages/Cart/CartSummary.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const CartSummary = ({ totalAmt, discountAmt, handleProceedToCheckout }) => {
+const CartSummary = ({ totalAmt, discountAmt, handleProceedToCheckout, shippingCharge }) => {
   return (
     <div className="flex justify-end mt-5">
       <div className="border p-6 bg-white shadow-md rounded-lg w-full md:w-1/2 lg:w-1/3">
@@ -12,6 +12,10 @@ const CartSummary = ({ totalAmt, discountAmt, handleProceedToCheckout }) => {
         <div className="flex justify-between mb-2 text-green-600">
           <span>Discount</span>
           <span>- ${discountAmt.toFixed(2)}</span>
+        </div>
+        <div className="flex justify-between mb-2">
+          <span>Shipping Charge</span>
+          <span>${shippingCharge.toFixed(2)}</span>
         </div>
         <div className="flex justify-between border-t pt-2 mt-2 font-bold">
           <span>Total</span>
