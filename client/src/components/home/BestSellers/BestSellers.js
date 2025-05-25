afde6476dfd58cc5b983102be5abf1cfbbcad0f1
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-25 18:35:10 +0530

    update infinite to true

--- client/src/components/home/BestSellers/BestSellers.js
+++ client/src/components/home/BestSellers/BestSellers.js
@@ -27,7 +27,7 @@ const BestSellers = () => {
   }, []);
 
   const settings = {
-    infinite: false, // Change to false to test
+    infinite: true, // Change to false to test
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 1,
