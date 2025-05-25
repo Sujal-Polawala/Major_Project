7da9eea7c7b2b3f69ee21cd821b447fe10adffbd
Sujal-Polawala <sujalpolawala@gmail.com>
2025-05-25 21:28:21 +0530

    Update app.py File

--- backend/app.py
+++ backend/app.py
@@ -246,4 +246,4 @@ def handle_tryon_request(data):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, debug=True)
