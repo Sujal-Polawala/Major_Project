eb0c3374876ea8d39cdde578a4f731336a9ecde3
Sujal-Polawala <sujalpolawala@gmail.com>
2025-06-04 18:14:31 +0530

    Change Localhost to Backend URL

--- admin/src/utils/axiosintance.js
+++ admin/src/utils/axiosintance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { setAccessToken, logoutSeller } from "../features/authSlice";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/",
+    baseURL: "https://trynbuy-admin.vercel.app/",
     withCredentials: true,
 });
 
