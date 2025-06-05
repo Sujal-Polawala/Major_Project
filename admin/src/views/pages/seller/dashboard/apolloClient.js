cbd7447037191a3a87590fbbb45677b8f80ef054
Sujal-Polawala <sujalpolawala@gmail.com>
2025-06-05 21:39:21 +0530

    Upated

--- admin/src/views/pages/seller/dashboard/apolloClient.js
+++ admin/src/views/pages/seller/dashboard/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "https://trynbuy-backend-myzl.onrender.com/graphql" }), // Your GraphQL Server
+  link: new HttpLink({ uri: "https://trynbuy-backend.onrender.com/graphql" }), // Your GraphQL Server
   cache: new InMemoryCache(),
 });
 
